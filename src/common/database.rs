824dbe8cfd40b2550af2b566180c2d88e09b9d3c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-03 00:01:54 +0800

    refactor: tweak the database url

--- src/common/database.rs
+++ src/common/database.rs
@@ -2,6 +2,6 @@ use diesel::PgConnection;
 use rust_wheel::config::db::config;
 
 pub fn get_connection() -> PgConnection{
-    let connection = config::connection("ALT_DATABASE_URL".to_string());
+    let connection = config::connection("TEX_DATABASE_URL".to_string());
     return connection;
 }
--- src/service/doc/doc_service.rs
+++ src/service/doc/doc_service.rs
@@ -1,4 +1,5 @@
 use diesel::QueryDsl;
+use log::error;
 use crate::common::database::get_connection;
 use crate::diesel::RunQueryDsl;
 use crate::model::diesel::tex::custom_tex_models::TexDoc;
@@ -7,9 +8,16 @@ pub fn get_doc_list(_tag: &String) -> Vec<TexDoc>{
     use crate::model::diesel::tex::tex_schema::tex_doc as cv_work_table;
     let query = cv_work_table::table.into_boxed::<diesel::pg::Pg>();
     let cvs = query
-        .load::<TexDoc>(&mut get_connection())
-        .expect("error get docs list");
-    return cvs;
+        .load::<TexDoc>(&mut get_connection());
+    match cvs {
+        Ok(result)=>{
+            return result;
+        },
+        Err(err) =>{
+            error!("get docs failed, {}", err);
+            return Vec::new();
+        }
+    }
 }
 
 
