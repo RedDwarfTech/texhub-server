a6c2d7a25f340688ff64917fc516b1ca6bbb2053
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-14 23:32:26 +0800

    fix: fix the url end by / issue

--- src/net/render_client.rs
+++ src/net/render_client.rs
@@ -10,9 +10,12 @@ use crate::model::{
     request::project::tex_compile_project_req::TexCompileProjectReq,
 };
 
-pub async fn render_request(params: &TexCompileProjectReq, proj: &TexProject) -> Option<serde_json::Value> {
+pub async fn render_request(
+    params: &TexCompileProjectReq,
+    proj: &TexProject,
+) -> Option<serde_json::Value> {
     let client = Client::new();
-    let url_path = format!("{}", "/render/compile/v1/project/");
+    let url_path = format!("{}", "/render/compil0e/v1/project");
     let url = format!("{}{}", get_app_config("render.render_api_url"), url_path);
     let file_path = format!("/opt/data/project/{}/{}", &params.project_id, proj.doc_name);
     let out_path = format!("/opt/data/project/{}", &params.project_id);
@@ -30,26 +33,42 @@ pub async fn render_request(params: &TexCompileProjectReq, proj: &TexProject) ->
         .await;
     match response {
         Ok(r) => {
-            let txt = r.text().await;
-            match txt {
-                Ok(info) => {
-                    info!("success:{}",info);
-                },
-                Err(e) => {
-                    error!("parse body error:{}",e);
-                },
+            if !r.status().is_success() {
+                error!(
+                    "send compile failed,status:{}, msg: {}",
+                    r.status(),
+                    r.text().await.unwrap()
+                );
+                return None;
             }
+            let resp: Result<serde_json::Value, reqwest::Error> = r.json().await;
+            match resp {
+                Ok(content) => {
+                    let result = content.get("result").unwrap();
+                    info!("compile request result,{}", result);
+                    return Some(result.clone());
+                }
+                Err(e) => {
+                    error!("get response failed: {}", e);
+                    return None;
+                }
+            }
+        }
+        Err(e) => {
+            error!("request compile error: {}", e);
             return None;
         }
-        Err(e) => { 
-            error!("request compile error: {}", e); 
-            return None;
-        },
     }
 }
 
 fn construct_headers() -> HeaderMap {
     let mut headers = HeaderMap::new();
+    let token: String = get_app_config("render.x_access_token").to_owned();
+    headers.insert("x-access-token", HeaderValue::from_str(&token).unwrap());
+    headers.insert("user-id", HeaderValue::from_static("1"));
+    headers.insert("app-id", HeaderValue::from_static("1"));
+    headers.insert("x-request-id", HeaderValue::from_static("reqwest"));
+    headers.insert("device-id", HeaderValue::from_static("reqwest"));
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
     headers
 }
