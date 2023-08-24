4c86eba75ca3537d2f1923c3038bc773ff73c4e8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-24 15:59:00 +0800

    refactor: tweak the length of project name

--- src/model/request/project/tex_project_req.rs
+++ src/model/request/project/tex_project_req.rs
@@ -2,7 +2,7 @@ use validator::Validate;
 
 #[derive(serde::Deserialize, Validate)]
 pub struct TexProjectReq {
-    #[validate(length(max = 2))]
+    #[validate(length(max = 256))]
     pub name: String,
     pub template_id: Option<i64>
 }
