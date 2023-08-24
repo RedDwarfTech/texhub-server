d79abc2e7fd663f2ba53d941c28b31ff6aee6475
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-24 15:23:31 +0800

    refactor: tweak the name of create project

--- src/model/request/project/tex_project_req.rs
+++ src/model/request/project/tex_project_req.rs
@@ -3,6 +3,6 @@ use validator::Validate;
 #[derive(serde::Deserialize, Validate)]
 pub struct TexProjectReq {
     #[validate(length(max = 2))]
-    pub doc_name: String,
+    pub name: String,
     pub template_id: Option<i64>
 }
