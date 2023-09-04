1387decdc8a80633dc3277f1687927593f2a1997
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-09-04 15:48:20 +0800

    feat: tweak the test get auth code

--- src/tests/test_proj.rs
+++ src/tests/test_proj.rs
@@ -2,11 +2,13 @@ use crate::controller::project::project_controller::get_temp_auth_code;
 use actix_web::{test, web, App};
 
 #[actix_rt::test]
-async fn test_compile_see() {
+async fn test_get_temp_auth_code() {
     let mut app =
         test::init_service(App::new().route("/", web::get().to(get_temp_auth_code))).await;
     let req = test::TestRequest::get().to_request();
-
     let resp = test::call_service(&mut app, req).await;
-    assert!(resp.status().is_success());
+    let status = resp.status().is_success();
+    let body = test::read_body(resp).await;
+    println!("Response body: {}", String::from_utf8_lossy(&body));
+    assert!(status);
 }
