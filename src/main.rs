df7f5b9997ccd9bdd220159bd354f53e3154463f
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-03 17:42:10 +0800

    refactor: tweak

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,15 @@
-extern crate openssl;
-#[macro_use]
-extern crate diesel;
+use actix_web::{HttpServer, App};
 
-use actix_web::{App, HttpServer};
-use controller::{
-    collar::collar_controller, file::file_controller, project::project_controller,
-    template::template_controller,
-};
-use monitor::health_controller;
-use rust_wheel::config::app::app_conf_reader::get_app_config;
-
-pub mod common;
-pub mod controller;
-pub mod model;
-pub mod monitor;
-pub mod net;
-pub mod service;
-pub mod tests;
+mod demo_controller;
+mod synctex;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
-    let port: u16 = get_app_config("texhub.port").parse().unwrap();
-    let address = ("0.0.0.0", port);
     HttpServer::new(|| {
         App::new()
-            .configure(collar_controller::config)
-            .configure(health_controller::config)
-            .configure(project_controller::config)
-            .configure(template_controller::config)
-            .configure(file_controller::config)
+            .configure(demo_controller::config)
     })
-    .bind(address)?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
 }
