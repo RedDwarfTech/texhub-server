ccfc5fee50a597023e53913bff4e8ce674b494a6
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-16 00:08:01 +0800

    refactor: return the editor src file name relative path

--- src/service/project/project_service.rs
+++ src/service/project/project_service.rs
@@ -1,9 +1,12 @@
+use crate::common::interop::synctex::synctex_node_tag;
+use crate::common::interop::synctex::synctex_node_visible_h;
+use crate::common::interop::synctex::synctex_node_visible_v;
+use crate::common::interop::synctex::synctex_scanner_get_name;
 use crate::common::interop::synctex::{
     synctex_display_query, synctex_edit_query, synctex_node_box_visible_depth,
     synctex_node_box_visible_h, synctex_node_box_visible_height, synctex_node_box_visible_v,
-    synctex_node_box_visible_width, synctex_node_column, synctex_node_line,
-    synctex_node_p, synctex_node_page, synctex_scanner_new_with_output_file,
-    synctex_scanner_next_result,
+    synctex_node_box_visible_width, synctex_node_column, synctex_node_line, synctex_node_p,
+    synctex_node_page, synctex_scanner_new_with_output_file, synctex_scanner_next_result,
 };
 use crate::diesel::RunQueryDsl;
 use crate::model::diesel::custom::file::file_add::TexFileAdd;
@@ -76,10 +79,7 @@ use std::process::{ChildStdout, Command, Stdio};
 use std::time::Duration;
 use tokio::sync::mpsc::UnboundedSender;
 use tokio::task;
-use crate::common::interop::synctex::synctex_node_visible_v;
-use crate::common::interop::synctex::synctex_node_visible_h;
-use crate::common::interop::synctex::synctex_node_tag;
-use crate::common::interop::synctex::synctex_scanner_get_name;
+use std::path::PathBuf;
 
 pub fn get_prj_list(_tag: &String, login_user_info: &LoginUserInfo) -> Vec<TexProject> {
     use crate::model::diesel::tex::tex_schema::tex_project as cv_work_table;
@@ -646,21 +646,19 @@ pub fn get_src_pos(params: &GetSrcPosParams) -> Vec<SrcPosResp> {
             file_path.clone(),
             proj_dir.clone()
         );
-        let tex_file_path = join_paths(&[proj_dir, params.path.clone(), params.file.to_string()]);
+        let tex_file_path = join_paths(&[proj_dir.clone(), params.path.clone(), params.file.to_string()]);
         let mut position_list: Vec<SrcPosResp> = Vec::new();
         let node_number = synctex_edit_query(scanner, params.page as c_int, params.h, params.v);
         if node_number > 0 {
             for _i in 0..node_number {
                 let node: synctex_node_p = synctex_scanner_next_result(scanner);
-                let file = synctex_scanner_get_name(scanner,synctex_node_tag(node));
-                warn!("page: {:?}", file);
+                let file = synctex_scanner_get_name(scanner, synctex_node_tag(node));
                 let line = synctex_node_line(node);
-                warn!("line: {:?}", line);
                 let column = synctex_node_column(node);
-                warn!("column: {:?}", column);
                 let c_str = CStr::from_ptr(file);
                 let file_name: String = c_str.to_string_lossy().into_owned();
-                let single_pos = SrcPosResp::from((file_name, line, column));
+                let src_relative_path = get_file_relative_path(file_name.clone(), proj_dir.clone());
+                let single_pos = SrcPosResp::from((src_relative_path, line, column));
                 position_list.push(single_pos);
             }
         }
@@ -670,6 +668,23 @@ pub fn get_src_pos(params: &GetSrcPosParams) -> Vec<SrcPosResp> {
             tex_file_path.clone()
         );
         return position_list;
+    }
+}
+
+fn get_file_relative_path(file_full_path: String, proj_dir: String) -> String {
+    let abs_path = Path::new(file_full_path.as_str());
+    let root = Path::new(proj_dir.as_str());
+    match abs_path.strip_prefix(root) {
+        Ok(relative) => {
+            let mut relative_path = PathBuf::from(relative);
+            let path_string = relative_path.as_mut_os_str().to_string_lossy().to_string();
+            let final_path = path_string.replace("./", "");
+            return final_path;
+        }
+        Err(err) => {
+            error!("Failed to get relative path: {}", err);
+            return "".to_owned();
+        }
     }
 }
 
