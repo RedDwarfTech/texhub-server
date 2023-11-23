8021d4d762e94d4a859d06066e52fd931bb2b804
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-11-24 00:09:23 +0800

    refactor: trim the search response quota

--- src/model/diesel/custom/file/search_file_resp.rs
+++ src/model/diesel/custom/file/search_file_resp.rs
@@ -13,11 +13,13 @@ pub struct SearchFileResp {
 
 impl SearchFileResp {
     pub(crate) fn new_file(file_map: Map<String,Value>) -> Self {
-        let f_name = format!("{}",file_map.get("name").unwrap().to_string());
+        let f_name = format!("{}",file_map.get("name").unwrap().to_string().trim_matches('"'));
+        let f_file_id = format!("{}",file_map.get("file_id").unwrap().to_string().trim_matches('"'));
+        let f_path = format!("{}",file_map.get("file_path").unwrap().to_string().trim_matches('"'));
         Self {
             name: f_name,
-            file_id: file_map.get("file_id").unwrap().to_string(),
-            file_path: file_map.get("file_path").unwrap().to_string(),
+            file_id: f_file_id,
+            file_path: f_path,
             content: file_map.get("content").unwrap().to_string()
         }
     }
