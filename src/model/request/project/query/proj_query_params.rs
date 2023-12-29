5489a796a16176abf42e90a5c4f3671cae151ed6
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-12-29 14:27:51 +0800

    feat: add trash filter when get project list

--- src/model/request/project/query/proj_query_params.rs
+++ src/model/request/project/query/proj_query_params.rs
@@ -3,7 +3,8 @@ pub struct ProjQueryParams {
     pub tag: Option<String>,
     pub role_id: Option<i32>,
     #[serde(default = "default_archive_status")]
-    pub archive_status: i32
+    pub archive_status: i32,
+    pub trash: Option<i32>
 }
 
 fn default_archive_status() -> i32 {
