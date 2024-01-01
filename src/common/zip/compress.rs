20da93da08b72d79c13b09d8a091c9345b21803e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-01-01 13:51:00 +0800

    feat: create archive folder if not exists

--- src/common/zip/compress.rs
+++ src/common/zip/compress.rs
@@ -1,4 +1,5 @@
 use std::{path::Path, fs::File};
+use rust_wheel::common::util::rd_file_util::create_folder_not_exists;
 use zip::{ZipWriter, write::FileOptions};
 use crate::service::global::proj::proj_util::get_proj_download_base_dir;
 
@@ -6,6 +7,7 @@ pub fn gen_zip(proj_id: &String) -> String {
     let file_folder = "/opt/data/project/2023/12/45e7bfd8344442049c22dd2e37f24ef6/";
     let folder_path = Path::new(file_folder);
     let download_dir = get_proj_download_base_dir(proj_id);
+    create_folder_not_exists(&download_dir);
     let archive_file_path = format!("{}{}",download_dir,"archive.zip");
     let file = File::create(archive_file_path.clone()).unwrap();
     let mut zip = ZipWriter::new(file);
