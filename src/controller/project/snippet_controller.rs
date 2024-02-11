de68895c68809d69d80f512eabb4cf3787ea0db4
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-02-11 15:51:39 +0800

    feat: tweak the snippet list url

--- src/controller/project/snippet_controller.rs
+++ src/controller/project/snippet_controller.rs
@@ -43,7 +43,7 @@ pub async fn del_snippet(
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/tex/snippet")
-            .route("/code", web::get().to(snippet_list))
+            .route("/list", web::get().to(snippet_list))
             .route("/edit", web::put().to(edit_snippet))
             .route("/del", web::delete().to(del_snippet)),
     );
