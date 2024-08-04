547aad40d4f30a988035a4161f918d14779c6cf3
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-08-04 12:03:26 +0800

    refactor: remove the content comma

--- src/model/diesel/custom/file/search_file_resp.rs
+++ src/model/diesel/custom/file/search_file_resp.rs
@@ -16,11 +16,12 @@ impl SearchFileResp {
         let f_name = format!("{}",file_map.get("name").unwrap().to_string().trim_matches('"'));
         let f_file_id = format!("{}",file_map.get("file_id").unwrap().to_string().trim_matches('"'));
         let f_path = format!("{}",file_map.get("file_path").unwrap().to_string().trim_matches('"'));
+        let f_content = format!("{}",file_map.get("content").unwrap().to_string().trim_matches('"'));
         Self {
             name: f_name,
             file_id: f_file_id,
             file_path: f_path,
-            content: file_map.get("content").unwrap().to_string()
+            content: f_content
         }
     }
 }
