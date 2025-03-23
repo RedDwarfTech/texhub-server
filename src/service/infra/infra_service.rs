29c4e76baeeb69d06be27fbe815bd6d1d7477f76
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-03-23 20:05:46 +0800

    refactor: remove unused code

--- src/service/infra/infra_service.rs
+++ src/service/infra/infra_service.rs
@@ -3,30 +3,10 @@ use log::error;
 use reqwest::blocking::Client;
 use rust_wheel::model::response::api_response::ApiResponse;
 
-pub fn get_uniq_id_legacy() -> Option<i64> {
-    let rt = actix_rt::Runtime::new().unwrap();
-   let handle = rt.spawn( async {
-        let id = get_snowflake_id();
-        id
-    });
-    return rt.block_on(handle).unwrap();
-}
-
 pub fn get_uniq_id() -> Option<i64> {
     return get_snowflake_id();
 }
 
-
-pub fn get_uniq_id_legacy_rt(){
-    // 构建一个 tokio 运行时： Runtime
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build().unwrap();
-
-    // 使用运行时来调用异步的连接方法
-    //let inner = rt.block_on(get_snowflake_id());
-    //inner
-}
 pub fn get_snowflake_id() -> Option<i64> {
     let client = Client::new();
     let infra_url = env::var("INFRA_URL").expect("INFRA_URL must be set");
