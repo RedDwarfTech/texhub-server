e63fb0da5e8a46595e952c55389abaf68fa5d172
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-07-20 20:12:46 +0800

    refactor: remove unused param

--- src/controller/project/queue/proj_queue_controller.rs
+++ src/controller/project/queue/proj_queue_controller.rs
@@ -1,12 +1,11 @@
+use crate::service::project::project_queue_service::update_expired_proj_queue;
 use actix_web::{web, HttpResponse};
 use rust_wheel::common::wrapper::actix_http_resp::box_actix_rest_response;
-use crate::{model::request::project::edit::edit_compile_queue_req::EditCompileQueueReq, service::project::project_queue_service::update_expired_proj_queue};
 
-async fn update_expired_queue_rec(_form: web::Json<EditCompileQueueReq>) -> HttpResponse {
+async fn update_expired_queue_rec() -> HttpResponse {
     update_expired_proj_queue();
     box_actix_rest_response("ok")
 }
-
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
